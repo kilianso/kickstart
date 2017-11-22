d6ffa094c98215cc1449d6b9e45d33d061bcf9ec
kilianso <hello@kilianso.com>
2017-11-22 11:24:17 +0100

    add demos and explanations on how to use ES6 modules

--- source/js/app.js
+++ source/js/app.js
@@ -1,3 +1,12 @@
 // import your stuff like this
 
+// Import an entire module. This runs the module's global code, but doesn't actually import any values.
 import './components/index.js'
+
+// This inserts myModule into the current scope, containing all the exports from the module in the file located in /components/index.js.
+import * as myModule from './components/index.js';
+
+// or cherry pick only the things you need:
+import { foo, bar} from './components/index.js';
+
+// avoid making function calls in here, put that in your modules.
--- source/js/components/index.js
+++ source/js/components/index.js
@@ -1,3 +1,40 @@
-// build your stuff and use ES6 modules
-// you can also import libraries from node modules like this:
-// import jquery from 'jquery'
+/*
+Build your stuff and use ES6 modules, no need of the old module loader and revealing module patterns.
+You can also import dependencies from node modules like this:
+
+import jquery from 'jquery'
+
+*/
+
+// There are multiple ways to write, like in old JS.
+
+// as declaration
+function foo() { console.log('yo') }
+
+// or expression
+var bar = () => {
+	console.log('bar');
+}
+
+// or evoke it immediately
+var foobar = (() => {
+  console.log('foobar');
+})()
+
+// Now export the stuff you want (make it accessible to other modules)
+
+// A module can only have one default export
+export default foobar;
+
+// but as many named exports as you'd like in curly braces.
+export { foo, bar };
+
+/* How to use that module, in other files like the app.js?
+
+// This inserts myModule into the current scope, containing all the exports from the module in the file located in ./components/index.js.
+import * as myModule from './components/index.js';
+
+// or cherry pick only the things you need:
+import { foo, bar} from './components/index.js';
+
+*/
